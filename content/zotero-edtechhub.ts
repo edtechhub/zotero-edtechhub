0f868c3897cacdc22621c9c9177b21f17f53383f
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2019-07-23 00:34:23 +0200

    collect archiveLocation

--- content/zotero-edtechhub.ts
+++ content/zotero-edtechhub.ts
@@ -9,6 +9,14 @@ function $patch$(object, method, patcher) {
   object[method][marker] = true
 }
 
+function getField(item, field) {
+  try {
+    return item.getField(field) || ''
+  } catch (err) {
+    return ''
+  }
+}
+
 $patch$(Zotero.Items, 'merge', original => async function(item, otherItems) {
   try {
     await Zotero.Schema.schemaUpdatePromise
@@ -29,6 +37,18 @@ $patch$(Zotero.Items, 'merge', original => async function(item, otherItems) {
 
     let user = Zotero.Prefs.get('sync.server.username')
     user = user ? `${user}, ` : ''
+
+    try {
+      const archiveLocation = getField(item, 'archiveLocation')
+      item.setField('archiveLocation', Array.from(new Set([item].concat(otherItems).map(i => getField(i, 'archiveLocation')).filter(al => al))).sort((a, b) => {
+        if (a === b) return 0
+        if (a === archiveLocation) return -1
+        if (b === archiveLocation) return 1
+        return a.localeCompare(b)
+      }).join(';'))
+    } catch (err) {
+      Zotero.debug('Cannot set archiveLocation on item: ' + err)
+    }
 
     const libraryKey = Zotero.URI.getLibraryPath(item.libraryID || Zotero.Libraries.userLibraryID).replace(/.*\//, '')
 
