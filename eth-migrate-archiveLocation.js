e0cef6258371549d3f3e3c976a953ed51c0581ea
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2020-04-13 20:23:43 +0200

    fixes #27

---
+++ eth-migrate-archiveLocation.js
@@ -0,0 +1,55 @@
+/*
+  * Open Tools-Developer-Run Javascript
+  * Check "Run as async function"
+  * Past this file into "Code"
+  * Click "run"
+
+  The results screen should now show the number of items modified
+*/
+
+let items = []
+for (const lib of libraries = Zotero.Libraries.getAll()) {
+  items = items.concat(await Zotero.Items.getAll(lib.libraryID))
+}
+
+const fieldID = {
+  archiveLocation: Zotero.ItemFields.getID('archiveLocation'),
+  extra: Zotero.ItemFields.getID('extra'),
+}
+
+async function moveArchiveLocation(item) {
+  if (! Zotero.ItemFields.isValidForType(fieldID.extra, item.itemTypeID)) return
+
+  let save = false
+  let extra = (item.getField('extra') || '').split('\n')
+  if (extra.length == 1 && !extra[0]) extra = []
+
+  if (Zotero.ItemFields.isValidForType(fieldID.archiveLocation, item.itemTypeID)) {
+    const archiveLocation = item.getField('archiveLocation') || ''
+    if (archiveLocation) {
+      item.setField('archiveLocation', '')
+      extra.push(`EdTechHub.ItemAlsoKnownAs: ${archiveLocation}`)
+      save = true
+    }
+  }
+
+  extra = extra.map(line => {
+    const renamed = line.replace(/^archiveLocation:/, 'EdTechHub.ItemAlsoKnownAs:')
+    save = save || renamed !== line
+    return renamed
+  })
+
+  if (save) {
+    item.setField('extra', extra.join('\n'))
+    await item.save()
+  }
+  return save
+}
+
+let saved = 0
+await Zotero.DB.executeTransaction(async () => {
+  for (const item of items) {
+    if (await moveArchiveLocation(item)) saved += 1
+  }
+})
+return saved
